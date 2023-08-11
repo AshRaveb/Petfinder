5c6647db0950b1a3c9df716869273be0e6b9c815
root <root@DESKTOP-KKU56CK>
2023-08-10 20:10:10 -0400

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // hello world route
@@ -21,6 +21,8 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    const pet = pets.find([pet => pet]);
+    res.send('All pets ${pet}');
 
 });
 
@@ -28,12 +30,12 @@ app.get('/api/v1/pets', (req, res) => {
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
 
-
+    const name = req.params.name;
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
-
+    
     // send the pet as a response
-
+    res.send('All names ${name}');
 });
 
 // get pet by owner with query string
@@ -43,9 +45,10 @@ app.get('/api/v1/pets/owner', (req, res) => {
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
+    const owner = req.params.owner;
 
     // send the pet as a response
-
+    res.send ('Owners are ${owner}');
 });
 
 app.listen(PORT, () => {
